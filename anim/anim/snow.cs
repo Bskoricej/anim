29e65348ea746934ccf40881f1ff5f195890765a
Bskoricej <113509044+Bskoricej@users.noreply.github.com>
2022-12-26 19:21:25 +0500

    anim

---
+++ anim/anim/snow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace anim
+{
+    internal class snow
+    {
+        int diametr;
+        int speed;
+        double angle;
+
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public snow (int diametr, int speed, double angle, int x, int y)
+        {
+
+            this.diametr = diametr;
+            this.speed = speed;
+            this.angle = angle;
+            X = x;
+            Y = y;
+        }
+
+        
+    }
+}
